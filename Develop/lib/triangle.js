3d5d306ffbc2005f5c4d29562819c4229e47c42f
miaciasMAC <miaciasullo@gmail.com>
2023-02-03 19:41:27 -0500

    built triangle shape

--- Develop/lib/triangle.js
+++ Develop/lib/triangle.js
@@ -1,7 +1,12 @@
 const Shape = require('./shape.js');
 
 class Triangle extends Shape {
-    
+    constructor(bgColor) {
+        super(bgColor);
+    }
+    render() {
+        return `<polygon xmlns="http://www.w3.org/2000/svg" x="100" y="10" points="150 10, 80 150, 220 150" fill="${this.bgColor}"/>`
+    }
 }
 
 module.exports = Triangle;
